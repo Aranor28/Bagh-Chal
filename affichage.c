ebc1fdc96b7660ef35b434a0b8466c3518f4ff1d
Milila <cannac.anais@hotmail.fr>
2014-04-03 21:16:36 +0200

    affichage depart

---
+++ affichage.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ncurses.h>
+#define TIGRE 1
+#define CHEVRE 2
+#define WIDTH 5
+#define HEIGT 5
+int plateau [WIDTH][HEIGT];
+void affichage_plateau(int nb_chevre_reserve){
+  int i,j;
+  clear();
+  start_color();
+  plateau[2][2]=CHEVRE;//J'ai mis une chèvre pour tester doduou <3
+  
+  /*Affichage du nombre de chèvres*/
+  init_pair(1, COLOR_BLACK,COLOR_CYAN);
+  attron(A_BOLD | COLOR_PAIR(1));
+  printw("%d Chevre(s) en réserve    %d Chevre(s) mangée(s)\n",20-nb_chevre_reserve,nb_chevre_reserve);
+  attroff(A_BOLD | COLOR_PAIR(1));
+  
+  /*Affichage de la grille*/
+  printw(" == == == == \n");
+  printw("|\\\\|//|\\\\|//|\n");
+  printw(" == == == == \n");
+  printw("|//|\\\\|//|\\\\|\n");
+  printw(" == == == == \n");
+  printw("|\\\\|//|\\\\|//|\n");
+  printw(" == == == == \n");
+  printw("|//|\\\\|//|\\\\|\n");
+  printw(" == == == == \n");
+  
+  /*Affichage des différents pions*/
+  for (i=0; i< HEIGT ;i++){
+     for (j=0 ; j < WIDTH ; j++){
+       
+	if ( plateau[i][j] == 0 ) // Si il n'y pas de pion
+	  mvprintw(i*2+1,j*3," ");
+	
+	else if ( plateau[i][j] == TIGRE ){ // Si il y a un tigre
+	  init_pair(2, COLOR_RED,COLOR_BLACK);
+	  attron(A_BOLD | COLOR_PAIR(2));
+	  mvprintw(i*2+1,j*3,"T");
+	  attroff(A_BOLD | COLOR_PAIR(2));
+	}
+	else { // Si il y a une chèvre
+	  init_pair(3, COLOR_GREEN,COLOR_BLACK);
+	  attron(A_BOLD | COLOR_PAIR(3));
+	  mvprintw(i*2+1,j*3,"C");
+	  attroff(A_BOLD | COLOR_PAIR(3));
+	}
+      }
+  }
+  getch();
+}
+
--- depart.c
+++ depart.c
@@ -1,9 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#define WIDTH 5
-#define HEIGT 5
-
-int plateau [WIDTH][HEIGT];
+#include "affichage.c"
 
 void initialiser_plateau (){
   int i,j;
@@ -12,43 +7,17 @@ void initialiser_plateau (){
       plateau[i][j]=0;
     }
   }
-  plateau[0][4]=1;
-  plateau[0][0]=1;
-  plateau[4][0]=1;
-  plateau[4][4]=1;
+  /*Placement des tigres dans chaque angles du plateau*/
+  plateau[0][4]=TIGRE;
+  plateau[0][0]=TIGRE;
+  plateau[4][0]=TIGRE;
+  plateau[4][4]=TIGRE;
 }
-
-void affichage_plateau(){
-  int i,j;
-    for (i=0; i< HEIGT ;i++){
-      printf("|");
-      for (j=0 ; j < WIDTH ; j++){
-	if ( plateau[i][j] == 0 )
-	  printf(" |");
-	else if ( plateau[i][j] == 1 )
-	  printf("T|");
-	else 
-	  printf("C|");
-      }
-      printf("\n");
-  }
-}
-
-void placer_chevre ( int x, int y){
-  if ( plateau[y][x] == 0)
-    plateau[y][x]=2;
-  else
-    printf("Erreur case déjà occupée\n");
-}
-
 int main (){
-  int x, y;
+  initscr();
+  curs_set(0);
   initialiser_plateau();
-  affichage_plateau();
-  printf("Entrez la position a laquelle vous voulez mettre la chèvre ");
-  scanf("%d",&x);
-  scanf("%d",&y);
-  placer_chevre(x,y);
-  affichage_plateau();
+  affichage_plateau(0);
+  endwin();
   return 0;
 }
