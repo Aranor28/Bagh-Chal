4a04a86d61d5c94f5cf09b7f1a6117eb4141373b
Milila <cannac.anais@hotmail.fr>
2014-04-27 17:34:35 +0200

    sauvegarde ok

--- sauvegarde.c
+++ sauvegarde.c
@@ -8,23 +8,35 @@
  * LE NOMBRE DE CHEVRES MANGEES
  * LE TOUR DU JOUEUR SUIVANT
  */
-void sauvegarde (int chevre_placee, int chevre_mangee, int joueur){
+void sauvegarde (int tigre_bloque, int phase, int joueur){
 	int i,j;
 
+printf("ici\n");
 	FILE * fich = fopen("sauvegarde.txt","w");
 	if (fich == NULL){
 		fprintf(stderr, "Erreur lors de la sauvegarde du plateau\n");
 		return;
 	}
-
+	fprintf(fich, "\\board \n");
 	for (i = 0; i < 5; i++){
-		for (j = 0; i < 5; j++)	{
-			fprintf(fich,"%d",plateau[i][j]);
+		for (j = 0; j < 5; j++)	{
+			if (plateau[i][j] == TIGRE)
+				fprintf(fich,"T");
+			else if (plateau[i][j] == CHEVRE)
+				fprintf(fich, "G");
+			else if (plateau[i][j] == VIDE)
+				fprintf(fich, ".");
 		}
 		fprintf(fich,"\n");
 	}
-	fprintf(fich,"%d\n",chevre_placee);
-	fprintf(fich,"%d\n",chevre_mangee);
-	fprintf(fich,"%d\n",joueur);
+	fprintf(fich, "\\endboard\n");
+	if (joueur == TIGRE)
+		fprintf(fich,"\\player T\n");
+	else
+		fprintf(fich,"\\player G\n");
+
+	fprintf(fich,"\\phase %d\n",phase);
+	fprintf(fich,"\\captured %d\n",tigre_bloque);
+	fclose(fich);
 	
 }
