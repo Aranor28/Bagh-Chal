9a267c782223eb93265d8e4e8cef6a565c981c64
Milila <cannac.anais@hotmail.fr>
2014-05-05 15:26:17 +0200

    doudouuuu

--- partie.h
+++ partie.h
@@ -1,10 +1,6 @@
 #ifndef PARTIE_H
 #define PARTIE_H
 
-#include <stdbool.h>
-
-void detection_fin_partie (int nb_chevre_mangee, int nb_tigre_bloque);
-int partie_detection_vainqueur ();
-bool partie_tigre_bloque (int x, int y);
+bool detection_fin_partie (int nb_chevre_mangee, int nb_tigre_bloque);
 
 #endif
