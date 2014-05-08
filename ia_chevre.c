3d6b8487187c580f501d26a2fe30de239b6607a8
adrian <adrian.amigues@gmail.com>
2014-05-08 20:46:48 +0200

    Adri : Améliore IA placement chèvres

--- ia_chevre.c
+++ ia_chevre.c
@@ -2,7 +2,8 @@
 #include "main.h"
 #include "tigre.h"
 #include "ia_chevre.h"
-
+#include <stdio.h>
+#include <ncurses.h>
 
 void ia_chevre_placement () {
 	int i,j;
@@ -10,7 +11,19 @@ void ia_chevre_placement () {
 		for (j=0 ; j < NB_CASES_Y ; j++){
 			if (plateau.grille[i][j] == VIDE && !ia_chevre_en_danger(i,j)) {
 				plateau.grille[i][j] = CHEVRE;
-				plateau.nb_chevres_placees = plateau.nb_chevres_placees + 1;
+				plateau.nb_chevres_placees++;
+				return;
+			}
+		}
+	}
+	/* Si aucun emplacement "sûr" n'à été trouvé 
+	 * On reboucle et on prend le premier disponible.
+	 * Moin coûteux que de mémoriser la première position disponible ??? */
+	for (i=0; i< NB_CASES_X ;i++){
+		for (j=0 ; j < NB_CASES_Y ; j++){
+			if (plateau.grille[i][j] == VIDE) {
+				plateau.grille[i][j] = CHEVRE;
+				plateau.nb_chevres_placees++;
 				return;
 			}
 		}
@@ -21,40 +34,31 @@ void ia_chevre_placement () {
 	et s'il peut manger la chevre */
 bool ia_chevre_en_danger (int x, int y) {
 	int i, j, depart_x, depart_y, fin_x, fin_y, x_final, y_final;
-	bool chevre_mangee = false;
+	bool acces_diag = ((x + y) % 2 == 0);
 
-	if (x == 0)
-		depart_x = 0;
-	else
-		depart_x = x - 1;
+	depart_x = (x == 0) ? 0 : x-1;
+	depart_y = (y == 0) ? 0 : y-1;
+	fin_x = (x == 4) ? 4 : x+1;
+	fin_y = (y == 4) ? 4 : y+1;
 
-	if (y == 0)
-		depart_y = 0;
-	else
-		depart_y = y - 1;
-
-	if (x == 5)
-		fin_x = 5;
-	else
-		fin_x = x + 1;
-
-	if (y == 5)
-		fin_y = 5;
-	else
-		fin_y = y + 1;
-
-	/* On recherche un tigre dans les cases adjacentes de la chevre */
-	for (i = depart_x; i < fin_x; i++){
-		for (j = depart_y; j < fin_y; j++){
-			if (plateau.grille[i][j] == TIGRE ){
-				x_final = 2 * x - i;
+	/* On recherche un tigre dans les cases adjacentes à la chevre */
+	for (i = depart_x; i <= fin_x; i++) {
+		for (j = depart_y; j <= fin_y; j++) {
+			if (plateau.grille[i][j] == TIGRE && (acces_diag || i == x || j == y)) {
+				x_final = 2 * x - i; // x + (x - i) || point + vecteur
 				y_final = 2 * y - j;
+
+				char temp[100];
+				sprintf(temp, "[%d, %d], tigre : %d, %d", x, y, x_final, y_final);
+				debug(temp, 0);
+				getch();
+
 				/* Si la case d'arrivée du tigre est valide */
 				if (x_final >= 0 && x_final <= 4 && y_final >= 0 && y_final <= 4 && plateau.grille[x_final][y_final] == VIDE)
-					chevre_mangee = true;
+					return(true);
 			}
 		}
 	}
-	return(chevre_mangee);
+	return(false);
 }	
 
